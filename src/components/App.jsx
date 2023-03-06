50a543dfc57a80f8c562c1dd10483f272c92fb0d
Ihor Melnykov <imelya17@gmail.com>
2023-03-06 23:45:56 +0200

    finished task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { useState } from "react";
+import { useLocalStorage } from "./hooks/Hooks";
+import { Form } from "./Form/Form";
+import { Filter } from "components/Filter/Filter";
+import { ContactsList } from "./ContactsList/ContactsList";
+import { Section, Title, Text } from "./App.styled";
+
+export function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+ const handleSubmit = data => {
+   if (contacts.find(contact => contact.name === data.name)) {
+     alert(`${data.name} is already in contacts`);
+     return;
+   }
+     setContacts(prevState =>  [...prevState, data]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+  
+  const filteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+  return filteredContacts; 
+  };
+
+  const deleteContacts = (contactId) => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== contactId));
+  };
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <Title>Phonebook</Title>
+      <Form onSubmit={handleSubmit}/>
+      <Title>Contacts</Title>
+      {contacts.length !== 0 ?
+       <>
+      <Filter value={filter} onChange={changeFilter}/>
+      <ContactsList contacts={filteredContacts()} onDeleteContact={deleteContacts} />
+       </> : 
+      <Text>There are no saved contacts!</Text>
+      }
+    </Section>
   );
 };
---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+import { ListUl, ListLi, Span, ListP, Button } from "./ContactsList.styled";
+
+export const ContactsList = ({contacts, onDeleteContact}) => {
+    return (
+        <ListUl>
+            { 
+            contacts.map(({id, name, number}) => {
+                return (
+                    <ListLi key={id}>
+                    <Span>{name}:</Span> 
+                    <ListP>{number}</ListP>
+                    <Button type='button' onClick={() => onDeleteContact(id)}>Delete</Button>
+                    </ListLi>
+                )
+            })}
+        </ListUl>
+    )}
+
+    ContactsList.propTypes = { 
+        contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired
+        }).isRequired
+        ).isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    };
